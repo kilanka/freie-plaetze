f5c95a31133d33f5765f350a06f921b5c266f375
bjoluc <mail@bjoluc.de>
2022-05-02 14:38:12 +0200

    Migrate backend env var handling to `@next/env`

--- backend/environment.ts
+++ backend/environment.ts
@@ -1,13 +1,13 @@
-import dotenv from "dotenv";
-dotenv.config();
+import {loadEnvConfig} from "@next/env";
+
+loadEnvConfig(process.cwd());
 
 export const isProduction = process.env.APP_ENV === "production";
-
-export const databaseUrl =
-	process.env.DATABASE_URL ?? "postgres://pguser:pgpass@localhost:5432/pguser";
+export const databaseUrl = process.env.DATABASE_URL!;
+export const nominatimSearchEndpoint = process.env.NOMINATIM_SEARCH_ENDPOINT!;
+export const imagesPath = process.env.IMAGES_PATH!;
 
 let secret = process.env.SESSION_SECRET;
-
 if (!secret) {
 	if (isProduction) {
 		throw new Error("The SESSION_SECRET environment variable must be set in production");
@@ -18,8 +18,3 @@ if (!secret) {
 
 export const sessionSecret = secret;
 export const sessionMaxAge = 60 * 60 * 24 * 30; // 30 days
-
-export const nominatimSearchEndpoint =
-	process.env.NOMINATIM_SEARCH_ENDPOINT ?? "https://nominatim.openstreetmap.org/search";
-
-export const imagesPath = process.env.IMAGES_PATH ?? "images";
