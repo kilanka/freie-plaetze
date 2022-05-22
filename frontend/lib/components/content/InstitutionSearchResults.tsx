4b90caec0711092a1b0d42e106b868cd2b622c82
bjoluc <mail@bjoluc.de>
2022-05-22 16:01:01 +0200

    Improve empty search result alerts

--- frontend/lib/components/content/InstitutionSearchResults.tsx
+++ frontend/lib/components/content/InstitutionSearchResults.tsx
@@ -5,7 +5,7 @@ import useInfiniteScroll from "react-infinite-scroll-hook";
 import {useSelector} from "react-redux";
 
 import {useSearchInstitutionsQuery} from "../../api/generated";
-import {selectSearchArgs} from "../../store/search";
+import {selectSearch, selectSearchArgs} from "../../store/search";
 import {LinkButton} from "../next/LinkButton";
 import {InstitutionListItem} from "./institution/InstitutionListItem";
 import {InstitutionStack} from "./InstitutionStack";
@@ -13,6 +13,7 @@ import {InstitutionStack} from "./InstitutionStack";
 const batchSize = 7;
 
 export const InstitutionSearchResults: React.FC = (props) => {
+	const search = useSelector(selectSearch);
 	const searchArgs = useSelector(selectSearchArgs);
 
 	const {loading, error, data, fetchMore} = useSearchInstitutionsQuery({
@@ -22,20 +23,19 @@ export const InstitutionSearchResults: React.FC = (props) => {
 	const institutions = data?.institutionSearchResults;
 	const institutionsCount = data?.institutionSearchResultsCount;
 
-	const isResultEmpty = !institutionsCount || !institutions;
-	const hasNextPage = !isResultEmpty && institutions.length < institutionsCount;
+	const hasNextPage =
+		institutionsCount !== 0 && (institutions?.length ?? 0) < (institutionsCount ?? 0);
 
 	const [sentryRef] = useInfiniteScroll({
 		loading,
 		hasNextPage,
-		disabled: isResultEmpty,
 		rootMargin: "0px 0px 400px 0px", // Start loading when the sentry is 400px away from the viewport
 		onLoadMore: async () => fetchMore({variables: {offset: institutions?.length ?? 0}}),
 	});
 
 	return (
 		<InstitutionStack>
-			{isResultEmpty && searchArgs.cityOrZip !== "" && (
+			{institutionsCount === 0 && (
 				<Alert
 					status="info"
 					alignSelf="center"
@@ -50,8 +50,17 @@ export const InstitutionSearchResults: React.FC = (props) => {
 					<AlertDescription maxWidth="md">
 						<Stack gap={2}>
 							<Box>
-								Im Umkreis von {searchArgs.radius} km um <b>{searchArgs.cityOrZip}</b> sind leider
-								keine Einrichtungen eingetragen. Betreiben Sie eine Einrichtung?
+								{search.areFiltersActive ? (
+									<>
+										Leider sind keine Einrichtungen eingetragen, die Ihren Suchkriterien
+										entsprechen. Betreiben Sie eine Einrichtung?
+									</>
+								) : (
+									<>
+										Im Umkreis von {search.radius} km um <b>{search.cityOrZip}</b> sind leider keine
+										Einrichtungen eingetragen. Betreiben Sie eine Einrichtung?
+									</>
+								)}
 							</Box>
 							<HStack alignSelf="center">
 								<LinkButton variant="solid" colorScheme="blue" href="/members/register">
