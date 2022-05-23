61b1201f61b7040452559ab6236998089addd372
bjoluc <mail@bjoluc.de>
2022-05-23 23:12:39 +0200

    Add site notice to about page

--- frontend/pages/about.tsx
+++ frontend/pages/about.tsx
@@ -1,13 +1,14 @@
-import {Container, Heading, List, ListIcon, ListItem} from "@chakra-ui/react";
+import {Container, Heading, List, ListIcon, ListItem, Text} from "@chakra-ui/react";
 import {NextPage} from "next";
 import React from "react";
 import {TiInputChecked} from "react-icons/ti";
 
 import {ParagraphLink} from "../lib/components/content/ParagraphLink";
 import {HeaderSection} from "../lib/components/HeaderSection";
+import {Link} from "../lib/components/next/Link";
 import {Title} from "../lib/components/Title";
 
-const HomePage: NextPage = () => {
+const Page: NextPage = () => {
 	return (
 		<>
 			<Title>Über Uns</Title>
@@ -32,11 +33,43 @@ const HomePage: NextPage = () => {
 				</List>
 			</HeaderSection>
 
-			<Container as="section" maxWidth="container.xl">
-				{/*  */}
+			<Container as="section" maxWidth="container.xl" mt={8}>
+				<Heading size="lg" mb={4}>
+					Impressum
+				</Heading>
+				<Text mb={8}>
+					Freie-Plaetze.de ist ein ehrenamtliches Projekt von Marcel Pohl (Konzept,
+					Öffentlichkeitsarbeit) und Björn Luchterhandt (technische Umsetzung). Der Quellcode ist
+					auf GitHub unter den Bedingungen der GNU General Public License v3.0 öffentlich zugänglich
+					(
+					<Link href="https://github.com/bjoluc/freie-plaetze" target="_blank">
+						https://github.com/bjoluc/freie-plaetze
+					</Link>
+					).
+				</Text>
+
+				<Heading size="md" mb={4}>
+					Angaben gemäß &sect; 5 TMG
+				</Heading>
+				<Text mb={4}>
+					Marcel Pohl
+					<br />
+					Mörikestraße 26
+					<br />
+					33100 Paderborn
+				</Text>
+				<Text mb={8}>E-Mail: info@freie-plaetze.de</Text>
+
+				<Heading size="md" mb={4}>
+					Verbraucherstreitbeilegung
+				</Heading>
+				<Text mb={8}>
+					Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer
+					Verbraucherschlichtungsstelle teilzunehmen.
+				</Text>
 			</Container>
 		</>
 	);
 };
 
-export default HomePage;
+export default Page;
